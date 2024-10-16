a8e54f66c470287021bbf4bffa5d892846e7e641
PulledIntheSky <75171834+PulledIntheSky@users.noreply.github.com>
2024-10-16 20:29:36 +0200

    Create main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI in your region
+  instance_type = "t2.micro"
+}
